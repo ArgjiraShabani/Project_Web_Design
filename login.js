5652332bc0eba2fe5461cdb3dda7d26322b978eb
Argjira369 <argjira.shabani@gmail.com>
2024-11-18 20:29:32 +0100

    commit

---
+++ login.js
@@ -0,0 +1,7 @@
+const form =document.getElementById('form');
+const username =  document.getElementsByClassName('name-register');
+const email= document.getElementsByClassName('email-login');
+const password = document.getElementsByClassName('password-login');
+const password2 = document.getElementsByClassName('password-login');
+
+form.addEventListener('')
