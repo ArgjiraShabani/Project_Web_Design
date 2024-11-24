25d7d13299343cdf9b2452e63e1d779baa650b6d
Argjira369 <argjira.shabani@gmail.com>
2024-11-24 01:08:04 +0100

    c

--- login.js
+++ login.js
@@ -1,7 +1,80 @@
 const form =document.getElementById('form');
-const username =  document.getElementsByClassName('name-register');
-const email= document.getElementsByClassName('email-login');
-const password = document.getElementsByClassName('password-login');
-const password2 = document.getElementsByClassName('password-login');
+const username=document.getElementById('name');
+const email=document.getElementById('email');
+const password=document.getElementById('password');
+const password2=document.getElementById('password2');
 
-form.addEventListener('')
+form.addEventListener('submit', e=> {
+    e.preventDefault();
+
+    validateInputs();
+});
+const setError =(element,message) => {
+    const inputBox = element.parentElement;
+    const errorDisplay = inputBox.querySelector('.error');
+    errorDisplay.innerText=message;
+    inputBox.classList.add('error');
+    inputBox.classList.remove('success');
+
+}
+const setSuccess = element =>{
+    const inputBox=element.parentElement;
+    const errorDisplay=inputBox.querySelector('.error');
+
+    errorDisplay.innerText='';
+    inputBox.classList.add('success');
+    inputBox.classList.remove('error');
+}
+const isValidEmail = email => {
+   const re =s = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>([\\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+   return re.test(String(email).toLowerCase());
+
+}
+
+const validateInputs =() => {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+    
+    if(username===''){
+        setError(username,'Username is required')
+    }
+    else{
+        setSuccess(username);
+
+    }
+   
+    if(emailValue === ''){
+        setError(username,'Email is required')
+
+    }else if(!isValidEmail(emailValue)){
+        setError(email,'Provide a valid email address');
+    }else{
+        setSuccess(email);
+    }
+    if(passwordValue === ''){
+        setError(password,'Password is required');
+
+    }else if(passwordValue.length <8){
+        setError(password,'Password must be at least 8 characters.');
+    }else{
+        setSuccess(password);
+
+    }
+    if(password2Value === ''){
+        setError(password2,'Please confirm your password');
+
+    }else if(password2Value!==passwordValue){
+        setError(password2,"Password doesn't match");
+    }else{
+        setSuccess(password2);
+    }
+
+
+
+
+    
+
+    
+};
