70cae1b17ec90d0bd4ced9f6ec44d17882c46218
AnitaMaloku <am70379@ubt-uni.net>
2025-01-01 16:28:14 +0100

    commit

--- flights.js
+++ flights.js
@@ -2,12 +2,12 @@
 function searchButtonClicked() {
   const searchTerm = document.getElementById('sInput').value.trim(); // Get the search input value
   if (searchTerm === "") {
-    alert("Please enter a search term.");
+    alert("You have not written anything to search for.Please write something!");
   
 }
 
 }
-function searchButtonClicked() {
+function searchButtonClickedd() {
   const people = document.getElementById('sFlight').value; // Get the search input value
   const roundtrip=document.getElementById("roundtrip").value;
   const des=document.getElementById("des").value;
