efaeedd6621e8a0e3dbdf9856bf93c69a983e992
arbenitaprebreza <arbenitaprebreza01@gmail.com>
2025-01-04 00:59:01 +0100

    Update contact.js

---
+++ contact.js
@@ -0,0 +1,8 @@
+
+function searchButtonClicked() {
+    const searchTerm = document.getElementById('sInput').value.trim(); // Get the search input value
+    if (searchTerm === "") {
+      alert("You have not written anything to search for.Please write something!");
+    
+    }
+  }
