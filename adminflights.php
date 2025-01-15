dc973e9469d26f52f8a19c7a4ad8d4ebfb383e7a
AnitaMaloku <am70379@ubt-uni.net>
2025-01-15 09:53:21 +0100

    commit

--- adminflights.php
+++ adminflights.php
@@ -11,14 +11,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="adminflights.css" rel="stylesheet" >
 </head>
 <body>
-    
-  
+    <div class="button">
+       <button type="button" ><a href="admin.php">Dashboard</a></button>
+    </div>
+  <div class="heading">
   <h2>Flights</h2>
+  
+  </div>
 
 
-<div>
+<div class="flighttable">
     <table>
         <thead>
             <tr>
@@ -31,23 +36,23 @@
            </tr>
         </thead>
            <tbody>
-                <?php
+            <?php
                     
                    
 
                     
-                    $sql1='Select flight.FlightID,  flight.DepID,flight.ArrivalID,flight.Depature_date,(SELECT TripName FROM trip WHERE TripID = flight.TripID) as Trip,flight.People,(SELECT AirportName FROM airports WHERE AirportID = flight.DepID) as DepartureAirport,
-                            (SELECT AirportName FROM airports WHERE AirportID = flight.ArrivalID) as ArrivalAirport
-                            FROM flight';
-                            
+            $sql1='Select flight.FlightID,  flight.DepID,flight.ArrivalID,flight.Depature_date,(SELECT TripName FROM trip WHERE TripID = flight.TripID) as Trip,flight.People,(SELECT AirportName FROM airports WHERE AirportID = flight.DepID) as DepartureAirport,
+                    (SELECT AirportName FROM airports WHERE AirportID = flight.ArrivalID) as ArrivalAirport
+                    FROM flight';
+                    
 
-                    $result =$conn->query($sql1);
-                    if(!$result){
-                        die("Query invalid:".$conn->error);
-                    }
+            $result =$conn->query($sql1);
+            if(!$result){
+                die("Query invalid:".$conn->error);
+            }
 
-                    while($row=$result->fetch_assoc()){
-                        echo "
+            while($row=$result->fetch_assoc()){
+                echo "
                         <tr>
                             
                         
@@ -63,14 +68,18 @@
                         </tr>
 
                     
-                        ";
-                    }
-                
+                    </tr>
+
+                    
+                    ";
+                }
             
-                ?>
-          </tbody>
         
-    </table>
-                </div>
+            ?>
+                        
+                     </tbody>
+        
+                 </table>
+    </div>
 </body>
 </html>
