b14e9d1869ff4f1832877bf82f7c3c03a44f0315
Argjira369 <argjira.shabani@gmail.com>
2025-01-30 20:55:34 +0100

    commit

--- logout.php
+++ logout.php
@@ -1,11 +1,24 @@
-<?php
-session_start();  
+/<?php
+/*session_start();  
 if(isset($_SESSION['Email'])){
 session_destroy();
 
 header("Location: login.php");
 }else{
     header('Location:login.php');
-}
+}*/
 
+?>
+
+<?php
+    session_start();
+    $_SESSION=[];
+    session_destroy();
+
+    if(isset($_COOKIE['login'])){
+        setcookie('login','',time()-3600,'/');
+    }
+
+    header('Location: Home.php');
+    exit();
 ?>
